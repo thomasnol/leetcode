ebb27128e965d04d9d509b2479e45b1ccfbc5190
Thomas Nolasque <114269812+thomasnol@users.noreply.github.com>
2025-07-26 21:31:07 -0400

    Time: 0 ms (100%), Space: 17.6 MB (97.31%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,31 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        # single stack solution
+        # go from left to right side
+        # for c in s:
+        # if c != ']':
+        # add to stack
+        # else:
+        # while stack.pop != '['
+        # pop stack 1 more time for the bracket
+        # extract multi-digit number
+        # push back into stack the multiplied string
+
+        stack = []
+        for c in s:
+            curstring = ''
+            if c != ']':
+                stack.append(c)
+            else:
+                curstring = ''
+                while stack[-1] != '[':
+                    curstring = stack.pop() + curstring
+                stack.pop()
+                mult = ''
+                while stack and stack[-1].isdigit():
+                    mult = stack.pop() + mult
+                curstring *= int(mult)
+                stack.append(curstring)
+        
+        return "".join(stack)
+                
