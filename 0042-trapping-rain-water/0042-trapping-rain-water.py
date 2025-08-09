8201533786499c84d36992ce3a7b3d886be0d9c7
Thomas Nolasque <114269812+thomasnol@users.noreply.github.com>
2025-08-08 23:14:51 -0400

    Time: 37 ms (8.3%), Space: 19.3 MB (38.26%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,35 +1,25 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # for a given height[x]
-        # find the max height from any nodes on the left
-        # do the same for the nodes on the right
-        # the trapped water in that column = min of both maximums - height[x]
-        left, right = 0, len(height) - 1
-        left_max, right_max = height[left], height[right]
-        count = 0
-        while left < right:
-            if height[left] < height[right]:
-                left_max = max(left_max, height[left])
-                # trap water in height[left]
-                count += left_max - height[left]
-                left += 1
-            else:
-                right_max = max(right_max, height[right])
-                # trap water in height[right]
-                count += right_max - height[right]
-                right -= 1
-        # better solution:
-        # while left + 1 < right:
-        #     if right_max > left_max:
-        #         left += 1
-        #         if height[left] > left_max:
-        #             left_max = height[left]
-        #         else:
-        #             count += left_max - height[left]
-        #     else:
-        #         right -= 1
-        #         if height[right] > right_max:
-        #             right_max = height[right]
-        #         else:
-        #             count += right_max - height[right]
-        return count
+        lmax = 0
+        rmax = 0
+        res = 0
+        # tempres = 0
+        prefix = [0] * len(height)
+        suffix = [0] * len(height)
+
+        prefix[0] = height[0]
+        for i in range(1, len(height)):
+            prefix[i] = max(prefix[i-1], height[i])
+        
+        
+        suffix[-1] = height[-1]
+        for i in range(len(height) - 2, -1, -1):
+            suffix[i] = max(suffix[i+1], height[i])
+        
+        # print(prefix)
+        # print(suffix)
+        # return 0
+        for i, c in enumerate(height):
+            tempres = min(prefix[i], suffix[i]) - height[i]
+            res += max(0, tempres)
+        return res
