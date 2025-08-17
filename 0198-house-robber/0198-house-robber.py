b4cb2714d1c378ae67f4bac7a14d621f79f55ca8
Thomas Nolasque <114269812+thomasnol@users.noreply.github.com>
2025-08-16 22:11:21 -0400

    Time: 0 ms (100%), Space: 17.7 MB (82.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -2,74 +2,93 @@ from functools import lru_cache
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # base cases are when have 0 or 1 houses left
-        # location is current index
-        loc = 0
         mapping = {}
+        mapping[len(nums)] = 0
+        mapping[len(nums) + 1] = 0
+        for i in range(len(nums) - 1, -1, -1):
+            rob = nums[i] + mapping[i + 2]
+            not_rob = mapping[i + 1]
+            mapping[i] = max(rob, not_rob)
+        return mapping[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#         # base cases are when have 0 or 1 houses left
+#         # location is current index
+#         loc = 0
+#         mapping = {}
         
-        def f(loc):
-            if loc + 1 == len(nums):
-                mapping[loc] = nums[loc]
-                return nums[loc]
-            if loc + 1 > len(nums):
-                mapping[loc] = 0
-                return 0
-            if loc in mapping:
-                return mapping[loc]
-            rob = nums[loc] + f(loc + 2)
-            not_rob = f(loc + 1)
-            res = max(rob, not_rob)
-            mapping[loc] = res
-            return res
+#         def f(loc):
+#             if loc + 1 == len(nums):
+#                 mapping[loc] = nums[loc]
+#                 return nums[loc]
+#             if loc + 1 > len(nums):
+#                 mapping[loc] = 0
+#                 return 0
+#             if loc in mapping:
+#                 return mapping[loc]
+#             rob = nums[loc] + f(loc + 2)
+#             not_rob = f(loc + 1)
+#             res = max(rob, not_rob)
+#             mapping[loc] = res
+#             return res
         
-        return f(0)
+#         return f(0)
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # @cache
-        # @lru_cache
-        lookup = {}
-        # i is the index of the house ur at
-        def f(i):
-            if i in lookup:
-                return lookup[i]
+        # # @cache
+        # # @lru_cache
+        # lookup = {}
+        # # i is the index of the house ur at
+        # def f(i):
+        #     if i in lookup:
+        #         return lookup[i]
             
-            if i >= len(nums):
-                return 0
+        #     if i >= len(nums):
+        #         return 0
             
-            rob = nums[i] + f(i+2)
-            not_rob = f(i+1)
+        #     rob = nums[i] + f(i+2)
+        #     not_rob = f(i+1)
 
-            lookup[i] = max(rob, not_rob)
+        #     lookup[i] = max(rob, not_rob)
 
-            return max(rob, not_rob)
+        #     return max(rob, not_rob)
 
-        return f(0)
+        # return f(0)
