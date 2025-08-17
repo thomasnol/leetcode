49d280c5fb1aefb5c1dc1d0e5990ef3080eb100a
Thomas Nolasque <114269812+thomasnol@users.noreply.github.com>
2025-08-16 21:55:58 -0400

    Time: 0 ms (100%), Space: 18 MB (12.77%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -2,12 +2,27 @@ from functools import lru_cache
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # have a dp array that has rob up to i ?
-        # dp = [0] * len(nums)
-        # dp[0] = 
-
-
-
+        # base cases are when have 0 or 1 houses left
+        # location is current index
+        loc = 0
+        mapping = {}
+        
+        def f(loc):
+            if loc + 1 == len(nums):
+                mapping[loc] = nums[loc]
+                return nums[loc]
+            if loc + 1 > len(nums):
+                mapping[loc] = 0
+                return 0
+            if loc in mapping:
+                return mapping[loc]
+            rob = nums[loc] + f(loc + 2)
+            not_rob = f(loc + 1)
+            res = max(rob, not_rob)
+            mapping[loc] = res
+            return res
+        
+        return f(0)
 
 
 
@@ -42,6 +57,7 @@ class Solution:
         # @cache
         # @lru_cache
         lookup = {}
+        # i is the index of the house ur at
         def f(i):
             if i in lookup:
                 return lookup[i]
