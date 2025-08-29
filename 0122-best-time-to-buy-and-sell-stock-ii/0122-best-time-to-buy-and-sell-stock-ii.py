b3a0eed4050d7c6ecb44813340eca9d5e2d76926
Thomas Nolasque <114269812+thomasnol@users.noreply.github.com>
2025-08-28 22:55:24 -0400

    Time: 0 ms (100%), Space: 18.8 MB (70.92%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,21 +1,63 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(1, len(prices)):
+            profit += max(prices[i] - prices[i - 1], 0)
+        
+        return profit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         # map (bool, int) -> int
-        mapping = {}
-        def f(own, day):
-            if day >= len(prices):
-                return 0
-            if (own, day) in mapping:
-                return mapping[(own, day)]
-            if own:
-                act = f(False, day + 1) + prices[day]
-            else:
-                act = f(True, day + 1) - prices[day]
+        # mapping = {}
+        # def f(own, day):
+        #     if day >= len(prices):
+        #         return 0
+        #     if (own, day) in mapping:
+        #         return mapping[(own, day)]
+        #     if own:
+        #         # sell
+        #         act = f(False, day + 1) + prices[day]
+        #     else:
+        #         # buy
+        #         act = f(True, day + 1) - prices[day]
                
-            not_act = f(own, day + 1)
-            max_profit = max(act, not_act)
-            mapping[(own, day)] = max_profit
-            return max_profit
-        return f(False, 0)
+        #     not_act = f(own, day + 1)
+        #     max_profit = max(act, not_act)
+        #     mapping[(own, day)] = max_profit
+        #     return max_profit
+        # return f(False, 0)
         
     
