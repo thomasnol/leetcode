aac0790ec64d70ffa58026a0a10f88474ec85c3f
Thomas Nolasque <114269812+thomasnol@users.noreply.github.com>
2025-09-05 23:23:12 -0400

    Time: 8 ms (52.44%), Space: 18.4 MB (8.04%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -7,24 +7,20 @@ class Solution:
         # we will split the problem, either choosing to cover with it or not
         # return value is True/False
         @cache
-        def f(s_idx, p_idx, mem):
-            
-            # if p[p_idx] != "*":
-            #     mem = p[p_idx]
-            
+        def f(s_idx, p_idx, mem):            
             # need to look ahead
             while p_idx + 1 < len(p) and p[p_idx + 1] == "*":
                 mem = p[p_idx]
                 p_idx += 1
-                print("MEM SET:", mem)
+                # print("MEM SET:", mem)
             
             # base case
             if s_idx >= len(s) and p_idx >= len(p):
-                print("BRANCH TRUE:", s_idx, p_idx, mem)
+                # print("BRANCH TRUE:", s_idx, p_idx, mem)
                 return True
             
             if p_idx < len(p) and p[p_idx] == "*":
-                print("Any match:", s_idx, p_idx, mem)
+                # print("Any match:", s_idx, p_idx, mem)
                 if s_idx < len(s) and (mem == s[s_idx] or mem == "."):
                     return f(s_idx, p_idx + 1, mem) or f(s_idx + 1, p_idx, mem) or f(s_idx + 1, p_idx + 1, mem)
                 else:
@@ -34,7 +30,7 @@ class Solution:
                 return False
 
             if (p[p_idx].isalpha() and s[s_idx] == p[p_idx]) or p[p_idx] == ".":
-                print("Standard match:", s_idx, p_idx, mem)
+                # print("Standard match:", s_idx, p_idx, mem)
                 return f(s_idx + 1, p_idx + 1, mem)
             
             
