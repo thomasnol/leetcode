a1933ac37216df94b6e3713c52883050ff756ef6
Thomas Nolasque <114269812+thomasnol@users.noreply.github.com>
2026-02-21 16:49:37 -0500

    Time: 0 ms (100%), Space: 19.3 MB (58.97%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,19 +1,23 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        # pop explored squares so that the cur matrix always represents all unexplored squares
-        ret = []
+        # edit matrix inplace
+        # right: entire first row
+        # down: last element of every row
+        # left: entire last row, reversed
+        # up: first element of every row, reversed
+        res = []
         while matrix:
-            # right side: entire first array
-            ret += matrix.pop(0)
-            # down side: last entry of every array
+            if matrix:
+                res += (matrix.pop(0))
+            
             if matrix and matrix[0]:
                 for row in matrix:
-                    ret.append(row.pop())
-            # left side: entire last array, in reverse order
+                    res.append(row.pop())
+            
             if matrix:
-                ret += (matrix.pop()[::-1])
-            # up side: first entry of every array, in reverse order
+                res += (matrix.pop()[::-1])
+            
             if matrix and matrix[0]:
                 for row in matrix[::-1]:
-                    ret.append(row.pop(0))
-        return ret
+                    res.append(row.pop(0))
+        return res
