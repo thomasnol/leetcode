5639c8656f5aac2e3fda42890b880dcb0471bb8f
Thomas Nolasque <114269812+thomasnol@users.noreply.github.com>
2026-08-14 15:38:14 -0400

    Time: 39 ms (28.38%), Space: 21.8 MB (84.73%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,16 +1,16 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         # hashmap, mapping specific subtotal we've reached, with frequency (how many times we've reached it)
-        # freqmap = defaultdict(int)
-        freqmap = Counter()
+        freqmap = defaultdict(int)
+        # freqmap = Counter()
         freqmap[0] = 1
         total = 0
         res = 0
 
-        for i, n in enumerate(nums):
+        for n in nums:
             total += n
             # print(total)
-            if freqmap[total - k]:
+            if total - k in freqmap:
                 res += freqmap[total - k]
             freqmap[total] += 1
         # print(freqmap)
